c48578ba98bb6873116a1bd0e844bc4f901a1d82
benjaminliang2 <benjaminliang2@gmail.com>
2022-11-02 15:23:21 -1000

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Keyboard, StyleSheet, TouchableWithoutFeedback, TouchableOpacity, Text, TextInput, View } from 'react-native';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  const [todaysCalorie, setTodaysCalorie] = useState(0)
+  const [calorie, setCalorie] = useState(100)
+
+  const addCalorie = () => {
+    setTodaysCalorie(prev => prev + parseInt(calorie))
+    setCalorie(0)
+
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss() }>
+
+      <View style={styles.container}>
+        <Text>{todaysCalorie}</Text>
+        <Text> Enter Calories </Text>
+        <TextInput
+          style={styles.input}
+          keyboardType={'numeric'}
+          onChangeText={setCalorie}
+          value={calorie}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={addCalorie}
+        >
+          <Text>Press Here</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -16,5 +41,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
